ee0cc28bc11e07e743e02f4a3f02184b5b7e7464
Jamie Hitchcock <jamiehitchcock@hotmail.co.uk>
2022-02-15 22:47:37 +0000

    Warping Text Function
    Added js function for warp text effect to h1 on hover.

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// function to warp the text on hover
+
+$(document).ready(function () {
+    let letters = $('h1').text();
+    let warped = '';
+    for (let letter of letters) {
+      warped += "<span class='warp'>" + letter + "</span>";
+    }
+    $('h1').html(warped);
+  });
